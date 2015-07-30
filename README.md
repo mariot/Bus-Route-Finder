61f78d5a421282634a9bbb44a6a0743d9d5041d2
Mariot Tsitoara <mariot.tsitoara@gmail.com>
2015-07-30 07:20:16 +0300

    Fixes #2 Added localization

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# bus-route-finder
-A web application that lets you find buses from A to B in Madagascar. 
+# Bus Route Finder
+An application for Firefox OS to help you find which buses to take to go to one place from another.
 
-Made with love with HTML5, JavaScript, AngularJS and Web Starter Kit.
+Made with love with HTML5, CSS3, JavaScript, AngularJS and Web Starter Kit.
+HTML5 application based purely on Web technologies. Application intended to be used in Firefox OS (should also work in Windows, Linux, Mac OS X and Android if Firefox browser installed).
 
 It uses JSON to store places and buses data.
 
@@ -12,4 +13,18 @@ JSON schema for the system:
 You enter two locations (from A to B) and the system first checks if it can get you there using one bus.
 If this fails, the system tries to find a bus running from A to a changeover location C, and then another bus from that changeover location to B.
 
-This software is released under the GNU GPL2.
+# Install from source
+
+* Download and extract content of this repository somewhere on local drive
+* Mozilla Firefox > Tools > Web Developer > App Manager
+* Add Packaged App
+* Choose directory with downloaded and extracted files
+* Start Simulator (If you do not have Firefox OS Simulator extension installed yet - you have to install it)
+* Press Update in App Manager
+* Click "Klif Mozika" icon on home screen inside simulator
+
+# Contribute
+Feel free to share some ideas for new features, report any issues, fork and contribute!
+
+# License
+GNU GPL v2 license, see LICENCE
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 var bus;
 var quartiers;
 
+document.getElementById('app-name').setAttribute('data-l10n-id', 'app-name');
+document.getElementById('city-label').setAttribute('data-l10n-id', 'city');
+
 $('#bouton_chercher').bind('click', afficherTrajet);
+$('#erreur').hide();
 
 function afficherTrajet() {
 	$('#ultrajet').empty();
-	$('#erreur').html('');
+	$('#erreur').hide();
 
 	if ($('#select_quartier_arrivee').val() != $('#select_quartier_depart').val()) {
 		calculTrajet($('#select_quartier_depart').val(), $('#select_quartier_arrivee').val())
@@ -31,7 +35,7 @@ function calculTrajet(departed, arriveed) {
 	//console.log(trajet);
 	
 	if (trajet.length == 0) {
-		$('#erreur').html('D&eacute;sol&eacute;, nous ne trouvons aucun bus pour votre trajet :(');
+		$('#erreur').show();
 	}
 
 	for (var j = 0; j < trajet.length; j++) {
@@ -61,18 +65,25 @@ function calculTrajet(departed, arriveed) {
 			
 			var h2nom = document.createElement("h1");
 			var h2nomJQ = $(h2nom);
-			h2nomJQ.html('Bus ' + trajet[j][i].nombus);
+			//h2nomJQ.html('Bus ' + trajet[j][i].nombus);
+			h2nom.setAttribute('data-l10n-id', 'bus');
+			h2nom.setAttribute('data-l10n-args', JSON.stringify({'bus-name': trajet[j][i].nombus}));
 			if (trajet[j][i].chemin == 'alternatif') {
-				h2nomJQ.html('OU Bus ' + trajet[j][i].nombus);
+				h2nom.setAttribute('data-l10n-id', 'bus-or');
+				h2nom.setAttribute('data-l10n-args', JSON.stringify({'bus-name': trajet[j][i].nombus}));
 			}
 			
 			var h3monter = document.createElement("h3");
 			var h3monterJQ = $(h3monter);
-			h3monterJQ.html('De ' + trajet[j][i].nomquartierdepart);
+			//h3monterJQ.html('De ' + trajet[j][i].nomquartierdepart);
+			h3monter.setAttribute('data-l10n-id', 'from');
+			h3monter.setAttribute('data-l10n-args', JSON.stringify({'place-name': trajet[j][i].nomquartierdepart}));
 			
 			var h3descendre = document.createElement("h3");
 			var h3descendreJQ = $(h3descendre);
-			h3descendreJQ.html('A ' + trajet[j][i].nomquartierarrivee);
+			//h3descendreJQ.html('A ' + trajet[j][i].nomquartierarrivee);
+			h3descendre.setAttribute('data-l10n-id', 'to');
+			h3descendre.setAttribute('data-l10n-args', JSON.stringify({'place-name': trajet[j][i].nomquartierarrivee}));
 			
 			h2nomJQ.appendTo(infodivJQ);
 			h3monterJQ.appendTo(infodivJQ);
